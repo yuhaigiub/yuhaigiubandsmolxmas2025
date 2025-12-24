4d7fea11a881cae8364519510417bbca42e2f1cd
yuhaigiub <buigiahuynt@gmail.com>
2025-12-24 14:22:27 +0900

    fix image path

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const TOTAL_NUMBERED_PHOTOS = 18;
 // 修改：将 top.jpg 加入到数组开头
 const bodyPhotoPaths = [
   '/photos/top.jpg',
-  ...Array.from({ length: TOTAL_NUMBERED_PHOTOS }, (_, i) => `/photos/${i + 1}.jpg`)
+  ...Array.from({ length: TOTAL_NUMBERED_PHOTOS }, (_, i) => `./photos/${i + 1}.jpg`)
 ];
 
 // --- 视觉配置 ---
